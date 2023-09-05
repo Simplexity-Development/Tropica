ae7de952d54de67fb91dbaa1b289f91dd4a20ea9
Peashooter101 <Peashooter101@yahoo.com>
2023-09-05 15:10:59 -0700

    Register Block Entities
    Forgot to do that, was leading to saving issues.

--- src/main/java/simplexity/tropica/Tropica.java
+++ src/main/java/simplexity/tropica/Tropica.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import simplexity.tropica.registries.TropicaBlock;
+import simplexity.tropica.registries.TropicaBlockEntity;
 import simplexity.tropica.registries.TropicaItem;
 
 public class Tropica implements ModInitializer {
@@ -17,6 +18,7 @@ public class Tropica implements ModInitializer {
         TropicaBlock.registerModBlocks();
         TropicaItem.registerModItems();
         TropicaBlock.registerCreativePlacements();
+        TropicaBlockEntity.registerBlockEntities();
         LOGGER.info("TROPICA IS LOADED YE");
     }
 }
