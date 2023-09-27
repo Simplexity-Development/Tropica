44397c052936369248093b81ce48c92667ae4bd9
RhythmicSys <mc.adhdmc@gmail.com>
2023-09-27 12:55:34 -0700

    Update textures

--- src/client/java/simplexity/tropica/TropicaClient.java
+++ src/client/java/simplexity/tropica/TropicaClient.java
@@ -62,6 +62,7 @@ public class TropicaClient implements ClientModInitializer {
 		BlockRenderLayerMap.INSTANCE.putBlock(TropicaBlock.BLACK_MANGROVE_PROPAGULE, RenderLayer.getCutout());
 		
 		BlockRenderLayerMap.INSTANCE.putBlock(TropicaBlock.BLACK_MANGROVE_DOOR, RenderLayer.getTranslucent());
+		BlockRenderLayerMap.INSTANCE.putBlock(TropicaBlock.BLACK_MANGROVE_TRAPDOOR, RenderLayer.getTranslucent());
 		BlockRenderLayerMap.INSTANCE.putBlock(TropicaBlock.COCONUT_DOOR, RenderLayer.getCutout());
 		
 		BlockRenderLayerMap.INSTANCE.putBlock(TropicaBlock.COCONUT_TUFT, RenderLayer.getCutout());
@@ -76,9 +77,6 @@ public class TropicaClient implements ClientModInitializer {
 	}
 
 	public void registerColorProviders() {
-		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0x5aed00, TropicaBlock.COCONUT_LEAVES);
-		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 0x5aed00, TropicaItem.COCONUT_LEAVES);
-
 		ColorProviderRegistry.BLOCK.register((state, view, pos, tintIndex) -> 0x92c648, TropicaBlock.BLACK_MANGROVE_LEAVES);
 		ColorProviderRegistry.ITEM.register((stack, tintIndex) -> 0x92c648, TropicaItem.BLACK_MANGROVE_LEAVES);
 	}
